4dd3f4a13313d0c300ac42aba78414ca450c71c7
Memesterz <31660848+Memesterz@users.noreply.github.com>
2017-11-03 10:20:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+window.onload(PasswordFunJS());
+
+
+function PasswordFunJS() {
+
+var PasswordCorrectJS = true;
+
+    while (PasswordCorrectJS === false) {
+    var PasswordJS = prompt("Enter The Password");
+        if (PasswordJS == "NoOneWillGuessThisPassword") {
+        PasswordCorrectJS = true;
+        } else{}
+    }
+}
+*/
